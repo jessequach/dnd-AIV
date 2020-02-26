d4f4dee463268a4984035cd1487fc570bd4b401c
jessequach <jesse.quach@yahoo.com>
2020-02-26 16:53:55 -0600

    * started working on character features for monk

--- functions/characterFeatures.js
+++ functions/characterFeatures.js
@@ -1,90 +1,59 @@
 module.exports = {
     features: function (classname) {
+        let featureNames
+        let featureDescription
+
         switch (classname) {
-            case "Barbarian":
-                let barbarianFeatureNames = [`Rage`, `Unarmored Defense`, `Danger Sense`, `Reckless Attack`,
-                    `Primal Path`, `Ability Score Improvement`, `Extra Attack`, `Fast Movement`, `Feral Instinct`,
-                    `Brutal Critical`, `Relentless Rage`, `Persistent Rage`, `Indomitable Might`, `Primal Champion`]
-                let barbarianFeatureData = [
-                    `On your turn, you can enter rage as a Bonus Action <br/>
-                While raging, gain the following benefits if you are not wearing heavy armor: <br/>
-                - Advantage on Strength Checks and Strength Saving Throws.
-                - +2 bonus to damage roll when using a melee weapon Attack using Strength. This bonus increases as you level.
-                - Resistance to bludgeoning, piercing, and slashing damage.
-                Cannot cast or concentrate on spellcasting while raging.
-                Rage lasts 1 minute and will end early from being <br/>
-                - knocked unconscious <br/>
-                - turn ends without attacking a hostile since your last turn or taken damage since then. <br/>
-                Can also end rage on turn as a Bonus Action.
-                Must Long Rest before being able to rage again after raging the maximum # of times depending on your level.
-                
-                <div class=container>
-                    <div class=row>
-                        <div class=col>Level</div>
-                        <div class=col>Maximum Rage</div>
-                    </div>
-                    <div class=row>
-                        <div class=col>1</div>
-                        <div class=col>2</div>
-                    </div>
-                    <div class=row>
-                        <div class=col>3</div>
-                        <div class=col>3</div>
-                    </div>
-                    <div class=row>
-                        <div class=col>6</div>
-                        <div class=col>4</div>
-                    </div>
-                    <div class=row>
-                        <div class=col>12</div>
-                        <div class=col>5</div>
-                    </div>
-                    <div class=row>
-                        <div class=col>17</div>
-                        <div class=col>6</div>
-                    </div>
-                </div>
+            case "Fighter": return [`testdata`]
+            case "Monk":
 
-                `,
-                    `Armor Class = (10 + dexMod + conMod) when not wearing armor (excluding Shield).
-                `,
-                    `At 2nd level, you can sense when things nearby shouldn't be.
-                `,
-                    `ra`,
-                    `pp`,
-                    `asi`,
-                    `ea`,
-                    `fm`,
-                    `fi`,
-                    `bc`, `rr`, `pr`, `im`, `pc`
+                featureNames = [
+                    `Unarmored Defense`,`Martial Arts`,              `Ki`,                 `Flurry of Blows`, 
+                    `Patient Defense`,  `Step of the Wind`,          `Unarmored Movement`, `Monastic Tradition`,
+                    `Deflect Missles`,  `Ability Score Improvement`, `Slow Fall`,          `Extra Attack`,
+                    `Stunning Strike`,  `Ki-Empowered Strikes`,      `Stillness of Mind`,  `Evasion`,
+                    `Purity of Body`,   `Tongue of the Sun and Moon`,`Diamond Soul`,       `Timeless Body`, 
+                    `Empty Body`,       `Perfect Soul`,              `Monastic Traditions`,
                 ]
+                featureDescription = [
+                    `While not wearing armor and not wielding a shield, AC equals (10 + monkDexMod + monkWisMod)`,
+                    `Mastery of Combat styles that use unarmed strikes and monk Weapons. <span id=&quot; popover-italics &quot;>(Shortswords/simple Melee Weapons that don't have 2h or heavy property)</span> <br/>
+                    Gain the following benefits while unarmed or wielding only Monk weapons while not wearing armor or Shield. <br/>
+                    • Can use Dex instead of Str for the Attack and Damage Rolls of your unarmed strikes and monk Weapons. <br/>
+                    • Can roll a d4 in place of the normal dage of Unarmed Strike or monk weapon. <br/>
+                    • When Attack action with an Unarmed Strike or Monk weapon on Your Turn, can make one Unarmed Strike as a Bonus Action. `,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                    ``,
+                ]
+                return createFeatures(featureNames, featureDescription)
+            case "Wizard": return [`testdata`]
+            case "Rogue": return [`testdata`]
 
-                return createFeatures(barbarianFeatureNames, barbarianFeatureData);
-
-            case "Wizard": return [`wizard`, `placeholder`]
         }
     }
 }
 
 function createFeatures(fNames, fData) {
     let features = []
-
-    // for (let index = fNames.length - 1; index >= 0; index--) {
-
-    //     let featureToAdd = `
-    //     <button 
-    //         type="button"
-    //         data-toggle="popover"
-    //         data-content="${fData[index]}">
-    //         ${fNames[index]}
-    //     </button>
-    //     `
-
-    //     console.log('fNames[index]', `${fNames[index]}`)
-
-    //     features.push(featureToAdd)
-    // }
-
     fNames.forEach((value, index) => {
         let featureToAdd = `
             <button 
@@ -95,8 +64,6 @@ function createFeatures(fNames, fData) {
                 ${fNames[index]}
             </button>
         `
-        console.log('fNames[index]', `${fNames[index]}`)
-
         features.push(featureToAdd)
     })
 
