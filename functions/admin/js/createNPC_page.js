62cd22bc6c17961c4dd453181904baa5f4c28f5f
jessequach <jesse.quach@yahoo.com>
2020-03-06 16:47:42 -0600

    * updating admin npc create page

--- functions/admin/js/createNPC_page.js
+++ functions/admin/js/createNPC_page.js
@@ -21,8 +21,8 @@ function createNPC_page_secured() {
                         <div class="input-group-prepend">
                             <label class="input-group-text" for="input_npcAlignment">Alignment</label>
                         </div>
-                            <select class="form-control" id="input_npcAlignment" onchange="checkAlignment()">
-                                <option value="" selected disabled hidden>-- Select Alignment --</option>
+                            <select class="form-control" id="input_npcAlignment">
+                                <option value="" selected>-- Select Alignment --</option>
                                 <option value="Friendly">Friendly</option>
                                 <option value="Neutral">Neutral</option>
                                 <option value="Hostile">Hostile</option>
@@ -31,12 +31,12 @@ function createNPC_page_secured() {
                 </div>
             </div>
 
-            <div id="professions" class="input-group" style="display: none;">
+            <div id="professions" class="input-group">
                 <div class="input-group-prepend">
                     <label class="input-group-text" for="input_profession">Profession</label>
                 </div>
-                    <select class="form-control" id="input_profession" onchange="checkProfession()">
-                        <option value="" selected disabled hidden>-- Select Profession --</option>
+                    <select class="form-control" id="input_profession">
+                        <option value="" selected>-- Select Profession --</option>
                         <option value="Aristocrat">Aristocrat</option>
                         <option value="Artisan">Artisan</option>
                         <option value="Beggar">Beggar</option>
@@ -47,25 +47,27 @@ function createNPC_page_secured() {
                         <option value="Miner">Miner</option>
                         <option value="Priest">Priest</option>
                         <option value="Shopkeeper">Shopkeeper</option>
+                        <option value="other">Other</option>
                     </select>
             </div>
 
-            <div id="shopList" class="input-group" style="display: none;">
+            <div id="shopList" class="input-group">
                 <div class="input-group-prepend">
                     <label class="input-group-text" for="input_shopRef">Shop</label>
                 </div>
                     <select class="form-control" id="input_shopRef">
                     <option value="" selected disabled hidden>-- Select Shop --</option>
+                    <option value="">None</option>
                     </select>
             </div>
 
-            <div class="row">
+            <div id="stats" class="row">
                 <div class="col">
                     <div class="input-group">
                         <div class="input-group-prepend">
                             <label class="input-group-text">STR</label>
                         </div>
-                        <input class="input_stat form-control" type="number" min="3" max="18" value="3" id="input_str"/>
+                        <input class="input_stat form-control" type="number" min="6" max="16" value="3" id="input_str"/>
                     </div>
                 </div>
                 <div class="col">
@@ -73,7 +75,7 @@ function createNPC_page_secured() {
                         <div class="input-group-prepend">
                             <label class="input-group-text">DEX</label>
                         </div>
-                        <input class="input_stat form-control" type="number" min="3" max="18" value="3" id="input_dex"/>
+                        <input class="input_stat form-control" type="number" min="6" max="16" value="3" id="input_dex"/>
                     </div>
                 </div>
                 <div class="col">
@@ -81,7 +83,7 @@ function createNPC_page_secured() {
                         <div class="input-group-prepend">
                             <label class="input-group-text">CON</label>
                         </div>
-                        <input class="input_stat form-control" type="number" min="3" max="18" value="3" id="input_con"/>
+                        <input class="input_stat form-control" type="number" min="6" max="16" value="3" id="input_con"/>
                     </div>
                 </div>
                 <div class="col">
@@ -89,7 +91,7 @@ function createNPC_page_secured() {
                         <div class="input-group-prepend">
                             <label class="input-group-text">INT</label>
                         </div>
-                        <input class="input_stat form-control" type="number" min="3" max="18" value="3" id="input_int"/>
+                        <input class="input_stat form-control" type="number" min="6" max="16" value="3" id="input_int"/>
                     </div>
                 </div>
                 <div class="col">
@@ -97,7 +99,7 @@ function createNPC_page_secured() {
                         <div class="input-group-prepend">
                             <label class="input-group-text">WIS</label>
                         </div>
-                        <input class="input_stat form-control" type="number" min="3" max="18" value="3" id="input_wis"/>
+                        <input class="input_stat form-control" type="number" min="6" max="16" value="3" id="input_wis"/>
                     </div>
                 </div>
                 <div class="col">
@@ -105,43 +107,34 @@ function createNPC_page_secured() {
                         <div class="input-group-prepend">
                             <label class=" input-group-text">CHA</label>
                         </div>
-                        <input class="input_stat form-control" type="number" min="3" max="18" value="3" id="input_cha"/>
+                        <input class="input_stat form-control" type="number" min="6" max="16" value="3" id="input_cha"/>
                     </div>
                 </div>
             </div>
 
-
+            <button class="btn btn-primary" onclick="randomizeStats()">Randomize Stats</button>
 
         </div>
     `
 }
 
-$(function () {
+$(document).ready(function () {
     getShopIDs()
 })
 
-function checkAlignment(){
-    let npcAlignment = $('#input_npcAlignment')
-    if (npcAlignment.val() === 'Neutral'){
-        $('#professions').show()
-    } else {
-        $('#professions').hide()
-        $('#shopList').hide()
-        $('#shopList').val('')
-    }
-    checkProfession()
+$(window).on('load', function () {
+    randomizeStats()
+})
+
+
+function randomStat(min, max) {
+    return Math.floor(Math.random() * (1 + max - min)) + min
 }
 
-function checkProfession() {
-    // Priest, Shopkeeper, Innkeeper
-    let professionShops = ['Shopkeeper', 'Innkeeper', 'Priest']
-    let npcProfession = $('#input_profession')
-
-    if (professionShops.includes(npcProfession.val())) {
-        $('#shopList').show()
-    } else {
-        $('#shopList').hide()
-        $('#shopList').val('')
+function randomizeStats() {
+    let stats = document.getElementsByClassName('input_stat')
+    for (let i = 0; i < stats.length; i++) {
+        stats[i].value = randomStat(6, 16)
     }
 }
 
